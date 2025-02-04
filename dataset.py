c4dd0bb463515e5ba2e060ca11bca6c6fe72eedd
sgchrist <sgchrist@ucsc.edu>
2025-02-04 13:55:30 -0800

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,34 @@
+# Just some sample data to get stuff running
+# I used:
+#        - CMS 22: Fl,P1,P2,P3,P4p,P5p,P6p,P8p (+8)
+#        - ATLAS 7: FL,S3,S4,S5,S7,S8 (+6)
+#        - LHCb 67: Rmue(x4) (+4)
+#        - LHCb 60: dBR/dq2(x5) (+5)
+#        Total: 23 observables, 4 measurements
+Measurements = ['CMS B0->K*mumu q2 1.1-2 November 2024','LHCb RK and RK* December 2022','ATLAS B0->K*mumu angular 4-6 May 2018','LHCb Bs->phimumu BR May 2021']
+
+
+Observables = [('<FL>(B0->K*mumu)',1.1,2),
+               ('<P1>(B0->K*mumu)',1.1,2),
+               ('<P2>(B0->K*mumu)',1.1,2),
+               ('<P3>(B0->K*mumu)',1.1,2),
+               ('<P4p>(B0->K*mumu)',1.1,2),
+               ('<P5p>(B0->K*mumu)',1.1,2),
+               ('<P6p>(B0->K*mumu)',1.1,2),
+               ('<P8p>(B0->K*mumu)',1.1,2),
+               ('<FL>(B0->K*mumu)',4.0,6.0),
+               ('<S3>(B0->K*mumu)',4.0,6.0),
+               ('<S4>(B0->K*mumu)',4.0,6.0),
+               ('<S5>(B0->K*mumu)',4.0,6.0),
+               ('<S7>(B0->K*mumu)',4.0,6.0),
+               ('<S8>(B0->K*mumu)',4.0,6.0),
+               ('<dBR/dq2>(Bs->phimumu)',0.10,0.98),
+               ('<dBR/dq2>(Bs->phimumu)',1.1,2.5),
+               ('<dBR/dq2>(Bs->phimumu)',2.5,4.0),
+               ('<dBR/dq2>(Bs->phimumu)',4.0,6.0),
+               ('<dBR/dq2>(Bs->phimumu)',15.0,19.0),
+               ('<Rmue>(B+->Kll)',1.1,6.0),
+               ('<Rmue>(B+->Kll)',0.1,1.1),
+               ('<Rmue>(B0->K*ll)',1.1,6.0),
+               ('<Rmue>(B0->K*ll)',0.1,1.1)
+               ]
